94f60225ee040093cb4c976dfb053327f0c997e1
Md. Rasheduzzaman Rajon <mrrajon76@gmail.com>
2022-03-23 13:37:10 +0600

    capital added

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -3,12 +3,13 @@ import './Country.css'
 
 const Country = (props) => {
     // console.log(props.country);
-    const { name, region, flags, area, population } = props.country;
+    const { name, flags, region, capital, area, population } = props.country;
     return (
         <div className='country-block'>
             <img src={flags.png} alt="" />
             <h2>{name.common}</h2>
             <p>Region: <span className='brand'>{region}</span></p>
+            <p>Capital: <span className='brand'>{capital}</span></p>
             <p>Area: <span className='brand'>{area} km<sup>2</sup></span></p>
             <p>Population: <span className='brand'>{population}</span></p>
         </div>
