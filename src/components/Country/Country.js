e48ef75eb4f6188482f1092c678f28f3242afbce
Md. Rasheduzzaman Rajon <mrrajon76@gmail.com>
2022-03-23 13:27:47 +0600

    title and favicon updated

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -9,7 +9,7 @@ const Country = (props) => {
             <img src={flags.png} alt="" />
             <h2>{name.common}</h2>
             <p>Region: <span className='brand'>{region}</span></p>
-            <p>Area: <span className='brand'>{area}</span></p>
+            <p>Area: <span className='brand'>{area} km<sup>2</sup></span></p>
             <p>Population: <span className='brand'>{population}</span></p>
         </div>
     );
